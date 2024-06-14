810390a45c7e38dabfbbb48387594eb64f47d728
MagickSpace <146006257+MagickSpace@users.noreply.github.com>
2024-06-15 02:51:31 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/fonts'
+  ],
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  routeRules: {
+    '/': { prerender: true }
+  },
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    strict: false
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
